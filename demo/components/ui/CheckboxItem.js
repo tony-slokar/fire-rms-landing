ae252f8eded6735a7dbccbc93cdb63d66fdd8ecd
Tony Slokar <121650738+tony-slokar@users.noreply.github.com>
2025-07-17 12:48:48 -0400

    Update CheckboxItem.js

--- demo/components/ui/CheckboxItem.js
+++ demo/components/ui/CheckboxItem.js
@@ -1,6 +1,6 @@
 const CheckboxItem = ({ label, checked = false }) => (
-    <div style={{ display: 'flex', alignItems: 'center', background: colors.white, padding: '12px', border: `1px solid ${colors.lightGray}`, borderRadius: '6px' }}>
-        <input type="checkbox" defaultChecked={checked} style={{ width: '20px', height: '20px', marginRight: '12px', accentColor: colors.primary }} />
-        <label>{label}</label>
+    <div style={{ display: 'flex', alignItems: 'center', background: 'var(--light)', padding: '12px', border: `1px solid var(--light-gray)`, borderRadius: '6px' }}>
+        <input type="checkbox" defaultChecked={checked} style={{ width: '20px', height: '20px', marginRight: '12px', accentColor: 'var(--primary)' }} />
+        <label style={{color: 'var(--dark)'}}>{label}</label>
     </div>
 );
