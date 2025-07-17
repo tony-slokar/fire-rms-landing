0c91c657b08fdf4fe14f804b8df56470da91d4d2
Tony Slokar <121650738+tony-slokar@users.noreply.github.com>
2025-07-17 12:48:20 -0400

    Update SubNav.js

--- demo/components/ui/SubNav.js
+++ demo/components/ui/SubNav.js
@@ -1,7 +1,7 @@
 const SubNav = ({ tabs, activeTab, setActiveTab }) => (
-    <div style={{ display: 'flex', borderBottom: `1px solid ${colors.lightGray}`, marginBottom: '25px', overflowX: 'auto' }}>
+    <div style={{ display: 'flex', borderBottom: `1px solid var(--light-gray)`, marginBottom: '25px', overflowX: 'auto' }}>
         {tabs.map(tab => (
-            <div key={tab} onClick={() => setActiveTab(tab)} style={{ padding: '10px 15px', borderBottom: activeTab === tab ? `3px solid ${colors.primary}` : '3px solid transparent', fontWeight: activeTab === tab ? 'bold' : 'normal', color: activeTab === tab ? colors.primary : colors.gray, cursor: 'pointer', fontSize: '14px', transform: 'translateY(1px)', whiteSpace: 'nowrap' }}>
+            <div key={tab} onClick={() => setActiveTab(tab)} style={{ padding: '10px 15px', borderBottom: activeTab === tab ? `3px solid var(--primary)` : '3px solid transparent', fontWeight: activeTab === tab ? 'bold' : 'normal', color: activeTab === tab ? 'var(--primary)' : 'var(--gray)', cursor: 'pointer', fontSize: '14px', transform: 'translateY(1px)', whiteSpace: 'nowrap' }}>
                 {tab}
             </div>
         ))}
